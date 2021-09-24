f4ff450b5684c252c30ad9d246c934d8abffd044
marianne111 <mariannec.matias@gmail.com>
2021-09-24 10:12:22 +0800

    Lazyload test fix

--- assets/app.js
+++ assets/app.js
@@ -37,3 +37,34 @@ document.getElementById("banner-title1").addEventListener("webkitAnimationEnd", 
 document.getElementById("banner-title2").addEventListener("webkitAnimationEnd", (ev) => {
   document.getElementById("banner-title2").classList.add("no-cursor");
 });
+
+/* Lazy load */
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyloadImages = document.querySelectorAll("img.lazy");    
+  var lazyloadThrottleTimeout;
+  
+  function lazyload () {
+    if(lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
+    }    
+    
+    lazyloadThrottleTimeout = setTimeout(function() {
+        var scrollTop = window.pageYOffset;
+        lazyloadImages.forEach(function(img) {
+            if(img.offsetTop < (window.innerHeight + scrollTop)) {
+              img.src = img.dataset.src;
+              img.classList.remove('lazy');
+            }
+        });
+        if(lazyloadImages.length == 0) { 
+          document.removeEventListener("scroll", lazyload);
+          window.removeEventListener("resize", lazyload);
+          window.removeEventListener("orientationChange", lazyload);
+        }
+    }, 20);
+  }
+  
+  document.addEventListener("scroll", lazyload);
+  window.addEventListener("resize", lazyload);
+  window.addEventListener("orientationChange", lazyload);
+});
