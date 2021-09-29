649e7ad0e8072eee137e7d047d2b35b617e3cd4b
marianne111 <mariannec.matias@gmail.com>
2021-09-29 18:03:21 +0800

    Lnading page Shopify

--- assets/lp_script.js
+++ assets/lp_script.js
@@ -1,3 +1,47 @@
+function closeDrawer() {
+  var element = document.getElementById("cart");
+  element.setAttribute('aria-hidden', true);
+  document.querySelector('.PageOverlay').classList.remove("is-visible");
+}
+
+function addToCart() {
+  var qty = 0;
+  var variant = "";
+  
+  document.querySelectorAll('.variant-titles').forEach(function(e) {
+    if (e.classList.contains('is-active')) {
+      variant = e.textContent;
+    }
+  });
+
+  if (variant == "Einzeln") {
+    qty = parseInt(document.getElementById("quantity_text_1").value) + 1;
+    document.getElementById("quantity_text_1").value = qty;
+    document.getElementById("variant1").classList.add("show");
+    document.querySelector('.Cart__ItemList').prepend(document.getElementById("variant1"));  
+  } else if (variant == "10% Rabatt") {
+    qty = parseInt(document.getElementById("quantity_text_2").value) + 1;
+    document.getElementById("quantity_text_2").value = qty;
+    document.getElementById("variant2").classList.add("show");
+    document.querySelector('.Cart__ItemList').prepend(document.getElementById("variant2"));
+  } else if (variant == "15% Rabatt") {
+    qty = parseInt(document.getElementById("quantity_text_3").value) + 1;
+    document.getElementById("quantity_text_3").value = qty;
+    document.getElementById("variant3").classList.add("show");
+    document.querySelector('.Cart__ItemList').prepend(document.getElementById("variant3"));
+  } else if (variant == "20% Rabatt") {
+    qty = parseInt(document.getElementById("quantity_text_4").value) + 1;
+    document.getElementById("quantity_text_4").value = qty;
+    document.getElementById("variant4").classList.add("show");
+    document.querySelector('.Cart__ItemList').prepend(document.getElementById("variant4"));
+  }
+
+  // open cart
+  var element = document.getElementById("cart");
+  element.setAttribute('aria-hidden', false);
+  document.querySelector('.PageOverlay').classList.add("is-visible");
+}
+
 document.querySelectorAll('.thumbnail_click').forEach(trigger => {
   trigger.onclick = function(e) {
     e.preventDefault();
@@ -30,8 +74,40 @@ document.querySelectorAll('.SizeSwatch__Radio').forEach(trigger => {
   };
 });
 
-function closeDrawer() {
-  var element = document.getElementById("cart");
-  element.setAttribute('aria-hidden', true);
-  document.querySelector('.PageOverlay').classList.remove("is-visible");
-}
+document.querySelectorAll('.CartItem__Remove').forEach(trigger => {
+  trigger.onclick = function(e) {
+    e.preventDefault();
+    var elem = trigger.getAttribute('data-elem');
+    var elem2 = trigger.getAttribute('data-qty');
+
+    document.getElementById(elem).classList.remove("show");
+    document.getElementById(elem2).value = 0;
+  };
+});
+
+document.querySelectorAll('.minus-btn').forEach(trigger => {
+  trigger.onclick = function(e) {
+    e.preventDefault();
+    var qty_elem = trigger.nextElementSibling;
+    var qty = parseInt(qty_elem.value) - 1;
+
+    if (qty > 0) {
+      qty_elem.value = qty;
+    } else {
+      var elem = trigger.getAttribute('data-elem');
+      var elem2 = trigger.getAttribute('data-qty');
+  
+      document.getElementById(elem).classList.remove("show");
+      document.getElementById(elem2).value = 0;
+    }
+  };
+});
+
+document.querySelectorAll('.plus-btn').forEach(trigger => {
+  trigger.onclick = function(e) {
+    e.preventDefault();
+    var qty_elem = trigger.previousSibling.previousSibling;
+    var qty = parseInt(qty_elem.value) + 1;
+    qty_elem.value = qty;
+  };
+});
